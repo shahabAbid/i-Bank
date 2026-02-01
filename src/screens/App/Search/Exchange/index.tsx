eb7d192298e3071cfbb23d4b529f8ecaf9c87ced
Wahab <wahababid524@gmail.com>
2026-02-01 16:36:14 +0500

    Exhange Screen Update

--- src/screens/App/Search/Exchange/index.tsx
+++ src/screens/App/Search/Exchange/index.tsx
@@ -1,98 +1,178 @@
-import { View, Text, TouchableOpacity, Image, Modal, FlatList } from 'react-native'
-import React, { useState } from 'react'
-import styles from './style'
-import { appIcons, appImages } from '../../../../shared/assets'
-import { AppInput } from '../../../../components/primitive/AppInput'
-import { WP } from '../../../../shared/exporter'
-
-
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Modal,
+  FlatList,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styles from './style';
+import { appIcons, appImages } from '../../../../shared/assets';
+import { AppInput } from '../../../../components/primitive/AppInput';
+import { WP } from '../../../../shared/exporter';
 
 const Exchange = ({ navigation }: any) => {
+  const [showmodal, setshowmodal] = useState(false);
+  const modaldata = [
+    {
+      id: 1,
+      tittle: 'VND ( Viet Nam Dong )',
+      code: 'VND',
+      item: { name: 'mohsin' },
+    },
+    { id: 2, tittle: 'USD ( Dollar )', code: 'USD' },
+    { id: 3, tittle: 'CNY (Yuan)', code: 'CNY' },
+    { id: 4, tittle: 'EUR ( Euro )', code: 'EUR' },
+    { id: 5, tittle: 'GBP ( British Pound )', code: 'GBP' },
+    { id: 6, tittle: 'RUB ( Russian Rubi)', code: 'RUB' },
+  ];
 
-    const [showmodal, setshowmodal] = useState(false)
-    const modaldata = [
-      {id:1, tittle: 'VND ( Viet Nam Dong )',},
-      {id:2, tittle: 'USD ( Dollar )',},
-      {id:3, tittle: 'CNY (Yuan)',},
-      {id:4, tittle: 'EUR ( Euro )',},
-      {id:5, tittle: 'GBP ( British Pound )',},
-      {id:6, tittle: 'RUB ( Russian Rubi)',},
-    ];
+  const [selecteditem, setselecteditem] = useState<any>('');
+  const [selectedCode, setSelectedCode] = useState<any>('USD');
+  const [fromToCode, setFromToCode] = useState('USD');
+  const [type, setType] = useState('');
+  const [Amount, setAmount] = useState('');
+  const [Amount2, setAmount2] = useState('');
 
-    const renderitem = ({ item }: any) => (
-      <View style={{width: '90%', alignSelf:'center', height: WP('10')}}>
-        <Text style={styles.textstyle}>{item.tittle}</Text>
-      </View>
-    )
+  const changecolor = (item: any) => {
+    setselecteditem(item);
+    if (type == 'to') {
+      setFromToCode(item.code);
+    } else {
+      setSelectedCode(item.code);
+    }
+    setshowmodal(false);
+  };
 
-    
+  useEffect(() => {
+    const fetchrates = async () => {
+      try {
+        const url = 'https://open.er-api.com/v6/latest/USD';
+        const res = await fetch(url);
+        const json = await res.json();
+        console.log('ya mera data', JSON.stringify(json, null, 2));
+      } catch (error) {
+        console.log('Error fetching exchange rates:', error);
+      }
+    };
+  }, []);
+
+  const renderitem = ({ item }: any) => {
+    return (
+      <TouchableOpacity onPress={() => changecolor(item)} activeOpacity={0.7}>
+        <View
+          style={{
+            width: '90%',
+            alignSelf: 'center',
+            height: WP('10'),
+            marginTop: WP('2'),
+          }}
+        >
+          <Text
+            style={
+              selecteditem?.id == item.id
+                ? styles.activetextstyle
+                : styles.textstyle
+            }
+          >
+            {item.tittle}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-       <View style={styles.wrapper}>
+      <View style={styles.wrapper}>
         <TouchableOpacity
-        activeOpacity={1}   
-        onPress={() => navigation.goBack('BottomNavigator')}>
-        <Image source={appIcons.BackIcon} style={styles.backIcon}/>
+          activeOpacity={1}
+          onPress={() => navigation.goBack('BottomNavigator')}
+        >
+          <Image source={appIcons.BackIcon} style={styles.backIcon} />
         </TouchableOpacity>
-        <Text style={styles.headingtext}>Exchange</Text> 
-    </View>
+        <Text style={styles.headingtext}>Exchange</Text>
+      </View>
 
       <View>
-        <Image source={appImages.Mainimg} style={styles.mainimg}/>
-        <View
-        style={styles.container2}
-        >
+        <Image source={appImages.Mainimg} style={styles.mainimg} />
+        <View style={styles.container2}>
           <AppInput
-          placeholder='Amount'
-          label='From'
-          isRequired={false}
-          editable={true}
-          labelstyle={styles.labelstyle}
-          rightText='USD'
-          rightIcon={<Image source={appIcons.InputArrow} style={{alignSelf: 'flex-end'}}/>}
-          inputStyle={styles.inputstyle}
-          rightIconContainerStyle={styles.righticoninputcontainer}
-          rightIconPress={() => setshowmodal(true)}
+            placeholder="0"
+            label="From"
+            isRequired={false}
+            editable={true}
+            keyboardType="numeric"
+            onChangeText={setAmount}
+            value={Amount}
+            labelstyle={styles.labelstyle}
+            rightText={selectedCode}
+            rightIcon={
+              <Image
+                source={appIcons.InputArrow}
+                style={{ alignSelf: 'flex-end' }}
+              />
+            }
+            inputStyle={styles.inputstyle}
+            rightIconContainerStyle={styles.righticoninputcontainer}
+            rightIconPress={() => {
+              setshowmodal(true), setType('from');
+            }}
           />
 
-          <Image source={appIcons.Arrow} style={styles.Arrowstyle}/>
+          <Image source={appIcons.Arrow} style={styles.Arrowstyle} />
 
           <AppInput
-          placeholder='Amount'
-          label='To'
-          isRequired={false}
-          editable={true}
-          labelstyle={styles.labelstyle}
-          rightText='USD'
-          rightIcon={<Image source={appIcons.InputArrow} style={{alignSelf: 'flex-end'}}/>}
-          inputStyle={styles.inputstyle}
-          rightIconContainerStyle={styles.righticoninputcontainer}
-          rightIconPress={() => setshowmodal(true)}
+            placeholder="0"
+            label="To"
+            isRequired={false}
+            editable={true}
+            labelstyle={styles.labelstyle}
+            rightText={fromToCode}
+            keyboardType="numeric"
+            onChangeText={setAmount2}
+            value={Amount2}
+            rightIcon={
+              <Image
+                source={appIcons.InputArrow}
+                style={{ alignSelf: 'flex-end' }}
+              />
+            }
+            inputStyle={styles.inputstyle}
+            rightIconContainerStyle={styles.righticoninputcontainer}
+            rightIconPress={() => {
+              setshowmodal(true), setType('to');
+            }}
           />
 
-          <Modal 
-          transparent
-          animationType='fade'
-          visible={showmodal}
-          onRequestClose={() => setshowmodal  (false)}
+          <View style={styles.wrapper4}>
+            <Text style={styles.currentText}>Currency rate</Text>
+            <Text style={styles.rateText}>1 USD = </Text>
+          </View>
+
+          <Modal
+            transparent
+            animationType="fade"
+            visible={showmodal}
+            onRequestClose={() => setshowmodal(false)}
           >
             <View style={styles.Overlay}>
               <View style={styles.modalbox}>
                 <Text style={styles.modalheading}>Select the currency</Text>
                 <FlatList
-                data={modaldata}
-                showsVerticalScrollIndicator={false}
-                keyExtractor={( item ) => item.id.toString()}
-                renderItem={renderitem}
+                  data={modaldata}
+                  showsVerticalScrollIndicator={false}
+                  keyExtractor={item => item.id.toString()}
+                  renderItem={renderitem}
                 />
               </View>
             </View>
           </Modal>
         </View>
       </View>
-
     </View>
-  )
-}
+  );
+};
 
-export default Exchange
+export default Exchange;
--- src/screens/App/Search/Exchange/style.tsx
+++ src/screens/App/Search/Exchange/style.tsx
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
-import { AppColors, AppFontsFamily, AppFontSize, WP } from '../../../../shared/exporter';
+import {
+  AppColors,
+  AppFontsFamily,
+  AppFontSize,
+  WP,
+} from '../../../../shared/exporter';
 import { Overlay } from 'react-native-maps';
-
 
 const styles = StyleSheet.create({
   container: {
@@ -16,39 +20,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-   headingtext:{
-      marginLeft: WP('5%'),
-      fontFamily: AppFontsFamily.Poppins_SemiBold,
-      fontSize: AppFontSize.TITLE2,
-      color: AppColors.Neutrals.DarkGray,
-    },
-
-    backIcon: {
-     marginLeft: WP('8%'),
-     tintColor: '#000',
+  headingtext: {
+    marginLeft: WP('5%'),
+    fontFamily: AppFontsFamily.Poppins_SemiBold,
+    fontSize: AppFontSize.TITLE2,
+    color: AppColors.Neutrals.DarkGray,
   },
 
-  cardstyle:{
+  backIcon: {
+    marginLeft: WP('8%'),
+    tintColor: '#000',
+  },
+
+  cardstyle: {
     width: '90%',
     alignSelf: 'center',
     height: WP('28'),
     backgroundColor: AppColors.Neutrals.White,
     borderRadius: 15,
     elevation: 5,
-    marginTop: WP('10')
+    marginTop: WP('10'),
   },
 
-  wrapper2:{
+  wrapper2: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
 
-  wrapper3:{
+  wrapper3: {
     flexDirection: 'column',
-    
   },
 
-  cardtext1:{
+  cardtext1: {
     fontSize: AppFontSize.TITLE3,
     fontFamily: AppFontsFamily.Poppins_SemiBold,
     color: AppColors.Neutrals.DarkGray,
@@ -56,68 +59,68 @@ const styles = StyleSheet.create({
     marginTop: WP('5'),
   },
 
-  cardtext2:{
+  cardtext2: {
     fontFamily: AppFontsFamily.Poppins_Medium,
     fontSize: AppFontSize.CAPTION2,
     color: AppColors.Neutrals.MediumGray,
     marginStart: WP('5'),
   },
 
-  imagestyle:{
+  imagestyle: {
     marginRight: WP('3'),
     marginTop: WP('4'),
   },
 
-  mainimg:{
+  mainimg: {
     alignSelf: 'center',
     marginTop: WP('5'),
   },
 
-  container2:{
+  container2: {
     width: '90%',
     height: WP('84'),
     borderRadius: 30,
     backgroundColor: AppColors.Neutrals.White,
     elevation: 5,
-    alignSelf:'center',
-    marginTop: WP('5')
-  },
-
-  Arrowstyle:{
     alignSelf: 'center',
     marginTop: WP('5'),
   },
 
-  labelstyle:{
+  Arrowstyle: {
+    alignSelf: 'center',
     marginTop: WP('5'),
   },
 
-  inputstyle:{
+  labelstyle: {
+    marginTop: WP('5'),
+  },
+
+  inputstyle: {
     width: '70%',
     fontSize: AppFontSize.BODY1,
     fontFamily: AppFontsFamily.Poppins_Medium,
     color: AppColors.Neutrals.DarkGray,
   },
 
-  righticoninputcontainer:{
-    backgroundColor: AppColors.Neutrals.White ,
-    borderLeftWidth: 2, 
-    borderLeftColor: AppColors.Neutrals.LightSilver, 
-    width: WP('20'), 
-    height: WP('8') , 
-    position: 'absolute', 
-    right: 10, 
-    justifyContent: 'center'
+  righticoninputcontainer: {
+    backgroundColor: AppColors.Neutrals.White,
+    borderLeftWidth: 2,
+    borderLeftColor: AppColors.Neutrals.LightSilver,
+    width: WP('20'),
+    height: WP('8'),
+    position: 'absolute',
+    right: 10,
+    justifyContent: 'center',
   },
 
-  Overlay:{
+  Overlay: {
     flex: 1,
-     backgroundColor: 'rgba(0,0,0,0.5)',
-    justifyContent:'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
     alignItems: 'center',
   },
 
-  modalbox:{
+  modalbox: {
     width: '90%',
     height: WP('70'),
     borderRadius: 15,
@@ -125,17 +128,44 @@ const styles = StyleSheet.create({
     elevation: 4,
   },
 
-  modalheading:{
+  modalheading: {
     fontSize: AppFontSize.TITLE3,
     fontFamily: AppFontsFamily.Poppins_SemiBold,
     color: AppColors.Neutrals.DarkGray,
-    alignSelf:'center',
+    alignSelf: 'center',
     marginTop: WP('5'),
   },
 
-  textstyle:{
+  textstyle: {
     fontSize: AppFontSize.BODY1,
     fontFamily: AppFontsFamily.Poppins_Medium,
-  }
+    color: '#979797',
+  },
+
+  activetextstyle: {
+    fontSize: AppFontSize.BODY1,
+    fontFamily: AppFontsFamily.Poppins_Medium,
+    color: AppColors.Primary.DarkBlue,
+  },
+  wrapper4: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: WP('3'),
+  },
+
+  currentText: {
+    fontSize: AppFontSize.BODY3,
+    fontFamily: AppFontsFamily.Poppins_Medium,
+    color: AppColors.Primary.DarkBlue,
+  },
+
+  rateText: {
+    fontSize: AppFontSize.BODY3,
+    fontFamily: AppFontsFamily.Poppins_Medium,
+    color: AppColors.Neutrals.DarkGray,
+    position: 'absolute',
+    right: 0,
+  },
 });
 export default styles;
